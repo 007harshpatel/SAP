06461ae3e74aee4770905f78d09023a4e2c31aca
Harsh Sojitra <sojitraharsh6233@gmail.com>
2024-08-29 18:18:53 +0530

    Step-27 Unit test with Qunit Step-28 Integration Test with OPA

--- webapp/test/initMockServer.js
+++ webapp/test/initMockServer.js
@@ -1,9 +1,11 @@
-sap.ui.define(["../localService/mockserver"], (mockserver) => {
-  "use strict";
+sap.ui.define([
+	"../localService/mockserver"
+], (mockserver) => {
+	"use strict";
 
-  // initialize the mock server
-  mockserver.init();
+	// initialize the mock server
+	mockserver.init();
 
-  // initialize the embedded component on the HTML page
-  sap.ui.require(["sap/ui/core/ComponentSupport"]);
+	// initialize the embedded component on the HTML page
+	sap.ui.require(["sap/ui/core/ComponentSupport"]);
 });
